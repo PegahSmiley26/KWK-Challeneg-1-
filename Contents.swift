47d1c1657fbcbe309f1a7d10e9856df3d2f2ed66
PegahSmiley26 <PegahSmiley@gmail.com>
2019-07-09 15:26:32 -0500

    Onitial Commit

---
+++ Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+
+//Intentions: find a func that finds out whether the number is an odd or even number.
+func evenNumb(Number : Int) {
+    if (Number % 2 == 0) {
+        print("\(Number) is even.")
+        
+    }
+
+}
+
+evenNumb(Number: 6)
+
